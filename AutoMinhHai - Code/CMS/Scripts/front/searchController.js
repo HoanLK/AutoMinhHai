5780c5175c83e79edde726fb9695359b1cd06544
pgdien <pgdien2@gmail.com>
2017-02-08 01:43:34 +0700

    Fix tìm kiếm
    Thêm sự kiện text trống cho nút tìm kiếm
    Chỉnh lại giao diện trang  kết quả tìm kiếm

--- AutoMinhHai - Code/CMS/Scripts/front/searchController.js
+++ AutoMinhHai - Code/CMS/Scripts/front/searchController.js
@@ -1,10 +1,17 @@
 ﻿frontApp.controller("searchController", ['$scope', '$http', '$window', '$cookieStore', function ($scope, $http, $window, $cookieStore) {
 
     $scope.search = function () {
+        if ($scope.searchText == null) {
+            alert('Bạn vui lòng nhập từ khóa cần tìm kiếm');
+        } else {
+            $cookieStore.put('search', $scope.searchText);
 
-        $cookieStore.put('search', $scope.searchText);
+            $window.location.href = '/tim-kiem';
+        }
 
-        $window.location.href = '/tim-kiem';
     };
 
+    $scope.test = function () {
+
+    }
 }]);
